13216cb50b06fc329015ead3f84fe0b3bf3b11aa
Marius Leahu <lmariusc@gmail.com>
2016-08-23 19:40:58 +0300

    Sample route with a Processor, a Bean and Camel commands over SSH

--- spring-boot-camel/src/main/java/com/mleahu/SpringBootCamelApplication.java
+++ spring-boot-camel/src/main/java/com/mleahu/SpringBootCamelApplication.java
@@ -1,12 +1,17 @@
 package com.mleahu;
 
+import org.apache.camel.spring.boot.CamelSpringBootApplicationController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootCamelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCamelApplication.class, args);
+		ApplicationContext applicationContext = new SpringApplication(SpringBootCamelApplication.class).run(args);
+		CamelSpringBootApplicationController applicationController = applicationContext
+				.getBean(CamelSpringBootApplicationController.class);
+		applicationController.run();
 	}
 }
